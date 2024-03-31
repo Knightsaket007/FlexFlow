ce55c4d386d76c7ebb082d6ef23885eb3019616a
Knightsaket007 <109225097+Knightsaket007@users.noreply.github.com>
2024-03-31 14:27:31 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -16,8 +16,8 @@ const cors = require('cors');
 
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:3000",
-  // origin: 'https://flexflow-css.netlify.app/',
+  // origin: "http://localhost:3000",
+  origin: 'https://flexflow-css.netlify.app/',
   credentials: true,
 }))
 app.use(fileUpload({
